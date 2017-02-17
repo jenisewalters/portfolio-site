0d89bb3e3a8b7465acc11914e283a652060043c9
jenisewalters <jenisewalters@Jenises-MacBook-Pro.local>
2017-02-17 12:49:31 -0500

    add resume button

--- wp-content/themes/portfolio-theme/templates/home.php
+++ wp-content/themes/portfolio-theme/templates/home.php
@@ -3,13 +3,18 @@
 <section class="hero">
   <div class="container">
     <div class="hero__text">
-      <?php  if (have_posts()) :
+      <?php if (have_posts()) :
               while (have_posts()) :
-                 the_post();
-                    the_content();
+                     the_post();
+                        the_content();
               endwhile;
             endif;
-      ?>
+
+            if (get_field('resume_url')) {
+              $resume_url = get_field('resume_url'); ?>
+              <a class="btn-primary" target="_blank" href="<?php echo $resume_url ?>">Download Resume</a>
+              <?php  } ?>
+
     </div>
   </div>
 </section>
@@ -18,34 +23,32 @@
 		<h2 class="title-border">Portfolio</h2>
 		  <div class="row">
   				<?php
-          $args = array(
-          'post_type' => array('portfolio'),
-          );
+            $args = array(
+            'post_type' => array('portfolio'),
+            );
 
-          $query = new WP_Query($args);
+            $query = new WP_Query($args);
 
           ?>
 					<?php while ($query->have_posts()) : $query->the_post(); ?>
 						<div class="col-md-6">
 							<div class="portfolio-tile">
                 <?php if (get_field('work_sample_url')) {
-                   $work_sample_url = get_field('work_sample_url');
-									} ?>
-  								<a href ="<?php echo $work_sample_url ?>" target="_blank" class="portfolio-tile__image">
-  								  <?php the_post_thumbnail('full', array('class' => 'img-responsive'));?>
-  								</a>
-								<a class="portfolio-tile__title title-border" href ="<?php echo $work_sample_url ?>" target="_blank"><?php the_title(); ?></a>
+                      $work_sample_url = get_field('work_sample_url');
+                      } ?>
+      								<a href ="<?php echo $work_sample_url ?>" target="_blank" class="portfolio-tile__image">
+      								  <?php the_post_thumbnail('full', array('class' => 'img-responsive'));?>
+      								</a>
+								      <a class="portfolio-tile__title title-border" href ="<?php echo $work_sample_url ?>" target="_blank"><?php the_title(); ?></a>
 									<div class="portfolio-tile__tags">
 										<?php
-                      $posttags = get_the_tags();
-                      if ($posttags) {
-                      foreach ($posttags as $tag) {
-                    ?>
-														<div class="tags__item"><?php echo $tag->name ?></div>
-												<?php
-                                                    }
-                                                }
-                                         ?>
+                        $posttags = get_the_tags();
+                        if ($posttags) {
+                            foreach ($posttags as $tag) {
+                        ?>
+												<div class="tags__item"><?php echo $tag->name ?></div>
+												<?php }
+                        } ?>
 								</div>
 							</div>
 						</div>
